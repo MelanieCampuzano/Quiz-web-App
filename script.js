609f284743cc1e0f911104f7ee98ec397ef32ab7
Melanie Campuzano <Undepth71@gmail.com>
2025-09-05 13:59:34 -0700

    Feat: sliding up answers

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 	$(".question").click(function() {
-		$(this).next().toggleClass("answer");	
+		$(this).next().toggleClass("answer");
+		$(this).next().slideToggle("Slow");
+		$(".answer").next().toggleClass("styling");
 	})
 })
